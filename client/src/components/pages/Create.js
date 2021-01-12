e03baead11679e8616ca12392a17b803e82953eb
An Jimenez <anjimenez@Ans-MacBook-Pro-2.local>
2021-01-12 14:04:19 -0800

    nav bar, routed create and read pages, small updated to utilties.css for font

---
+++ client/src/components/pages/Create.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+import "../../utilities.css";
+import "./Create.css";
+
+class Create extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Create Page</h1>
+      </>
+    );
+  }
+}
+
+export default Create;
---
+++ client/src/components/pages/Read.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+import "../../utilities.css";
+import "./Read.css";
+
+class Read extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Read Page</h1>
+      </>
+    );
+  }
+}
+
+export default Read;
