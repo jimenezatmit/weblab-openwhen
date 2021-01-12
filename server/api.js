43f62ee43b30ee60b1be3bc469f39143ce7e9df5
kmpearce <65831324+kmpearce@users.noreply.github.com>
2021-01-12 16:31:55 -0500

    attempting api.js post requests for letters and packages

--- server/api.js
+++ server/api.js
@@ -11,6 +11,9 @@ const express = require("express");
 
 // import models so we can interact with the database
 const User = require("./models/user");
+//added by Kate
+const Letter  = require("./models/letter");
+const Package = require("./models/package");
 
 // import authentication library
 const auth = require("./auth");
@@ -41,6 +44,28 @@ router.post("/initsocket", (req, res) => {
 // |------------------------------|
 // | write your API methods below!|
 // |------------------------------|
+
+//jan 12 Kate  attempting post letter, no idea what I am doing
+router.post("/letter", (req, res) => {
+  const newLetter = new Letter({
+    open_date: req.body.open_date,
+    message: req.body.message,
+    _id: req.body._id
+  });
+
+  newLetter.save().then((letter) => res.send(letter));
+});
+//attempting post package
+router.post("/package", (req, res) => {
+  const newPackage = new Package({
+    _id: req.user._id,
+    letter_ids: req.body.letter_ids,
+    sender_name: req.user.name,
+    recipient_email = req.body.recipient_email,
+  });
+
+  newPackage.save().then((package) => res.send(package));
+});
 
 // anything else falls to this "not found" case
 router.all("*", (req, res) => {
