ca43dbe6604f13bd117015776547bdf059cdf186
An Jimenez <anjimenez@Ans-MacBook-Pro-2.local>
2021-01-13 10:10:00 -0800

    thank you pages

---
+++ client/src/components/pages/ThankYou.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+
+import "../../utilities.css";
+import "./ThankYou.css";
+
+class ThankYou extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <h1 className="Thank-title u-textCenter">
+          Thank you for using Open When! Your letters have been sent.
+        </h1>
+        <div className="buttonContainer">
+          <button type="button" className="Thank-button Thank-description">
+            <Link to="/" className="Thank-link">
+              return home
+            </Link>
+          </button>
+          <button type="button" className="Thank-button Thank-description">
+            <Link to="/create" className="Thank-link">
+              send another package
+            </Link>
+          </button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default ThankYou;
