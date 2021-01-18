968e800bd6488adfa08fb17ddace310e7eb9e96d
kmpearce <65831324+kmpearce@users.noreply.github.com>
2021-01-18 16:24:59 -0500

    changing home page text to better instructions + faq

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
   }
 
   componentDidMount() {
-    document.title = "Open When";
+    document.title = "Open When: Home";
   }
 
   render() {
@@ -18,8 +18,7 @@ class Home extends Component {
       <>
         <h1 className="Home-title u-textCenter">open when...</h1>
         <div className="Home-description">
-          send a package of virtual letters for special moments in their life (or another
-          tagline/summary)
+          send a package of virtual letters with custom unlocking dates 
         </div>
         <div className="u-textCenter">
           <button type="button" className="Home-button Home-description">
@@ -33,27 +32,59 @@ class Home extends Component {
             </Link>
           </button>
         </div>
-        <h1 className="Home-title u-textCenter">how it works</h1>
+        <h1 className="Home-title u-textCenter Home-blueText" >how it works</h1>
         <hr className="Home-line" />
-        <h4 className="Home-subTitle">step one: account setup and create</h4>
+        <h4 className="Home-subTitle">step one: create package</h4>
         <div className="Home-description">
-          create an account, and navigate to the create page where you can select the number of
-          letters and prompts.
+          navigate to the "create" page where you can input your name and the recipient's email
         </div>
         <hr className="Home-line" />
         <h4 className="Home-subTitle">step two: write letters and customize tags</h4>
         <div className="Home-description">
-          for each letter added to your package, select the "open when" date (either a specific date
-          or forever) and write your letter in the fields provided.
+          for each letter added to your package, select the "open when" date and write your message
         </div>
         <hr className="Home-line" />
         <h4 className="Home-subTitle">step three: send letters!</h4>
         <div className="Home-description">
-          hit send letters at the bottom of the create page. your recipient will receive a
-          notification about their package of letters, and they'll be able to open each one when
-          their "open when" date arrives.
+          hit "all done, send letters" button at the bottom of the "create" page to send package
         </div>
         <hr className="Home-line" />
+        <h4 className="Home-subTitle">step four: recipient views letters! </h4>
+        <div className="Home-description">
+          the recipient receives an email with a link and code to view their letters 
+        </div>
+        <br></br>
+        <h1 className="Home-title u-textCenter Home-blueText">faq</h1>
+        <hr className="Home-line" />
+        <h4 className="Home-subTitle">what is a package? what is a letter?</h4>
+        <div className="Home-description">
+          each letter has a prompt, "open when" date, and a message. when you make multiple letters at once for the same person, they are sent as a package via one email that has a link to view them all.
+        </div>
+        <hr className="Home-line" />
+        <h4 className="Home-subTitle">what is an "open when" date?</h4>
+        <div className="Home-description">
+          a letter's message only becomes visible upon the "open when" date, but you can also set the date to 00/00/0000 for it to be unlocked from the get-go
+        </div>
+        <hr className="Home-line" />
+        <h4 className="Home-subTitle">what will the recipient see if they try to view the letter before the "open when" date?</h4>
+        <div className="Home-description">
+          clicking on the envelope and prompt will take them to the letter-viewing page, but it will tell them when it will be unlocked instead of showing them the message
+        </div>
+        <hr className="Home-line" />
+        <h4 className="Home-subTitle">why can't the recipient find the email?</h4>
+        <div className="Home-description">
+          it's in their junk inbox :( sender is fromopenwhen@gmail.com
+        </div>
+        <hr className="Home-line" />
+        <h4 className="Home-subTitle">why won't it let me click the button?</h4>
+        <div className="Home-description">
+          the form requires certain formatting or it won't let you move on, check the instructions and try again (note: MM/DD/YYYY for date!)
+        </div>
+        <hr className="Home-line" />
+        <h4 className="Home-subTitle">why did you make this site?</h4>
+        <div className="Home-description">
+          we are An and Kate, two gals who believe that small acts of kindness and thoughtful messages can brighten people's lives. we hope that this simple site encourages you to support, inspire, encourage, and celebrate your friends, family, employees, coworkers, or anyone at all! 
+        </div>
       </>
     );
   }
