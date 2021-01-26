8f7d51055bd5cf1c84fb1bf916b2de300c9f880b
kmpearce <65831324+kmpearce@users.noreply.github.com>
2021-01-26 15:19:30 -0500

    improved how it works content, purple fixed footer

--- client/src/components/pages/HowItWorks.js
+++ client/src/components/pages/HowItWorks.js
@@ -15,37 +15,23 @@ class HowItWorks extends Component {
         <h1 className="Home-title u-textCenter u-margin">how it works</h1>
         <div class="sidenav">
           <a href="#">how it works</a>
+          <a href = "#">faq</a>
           <a href="#">about us</a>
         </div>
         <hr className="Home-line" />
         <h4 className="Home-subTitle Home-blueText">1. login</h4>
         <div className="Home-description">
-          you must log in with google account in upper right corner to unlock site features
+          press "get started" and log in with Google
         </div>
         <hr className="Home-line" />
         <h4 className="Home-subTitle Home-blueText">2. create package</h4>
         <div className="Home-description">
-          navigate to the "create" page where you can input your name and the recipient's email
+          navigate to the "create" page and follow steps to create/edit/send letters
         </div>
         <hr className="Home-line" />
-        <h4 className="Home-subTitle Home-blueText">3. write letters and customize tags</h4>
+        <h4 className="Home-subTitle Home-blueText">3. recipient views letters </h4>
         <div className="Home-description">
-          for each letter added to your package, select the "open when" date and write your message
-        </div>
-        <hr className="Home-line" />
-        <h4 className="Home-subTitle Home-blueText">4. send letters!</h4>
-        <div className="Home-description">
-          hit "all done, send letters" button at the bottom of the "create" page to send package
-        </div>
-        <hr className="Home-line" />
-        <h4 className="Home-subTitle Home-blueText">5. recipient gets email! </h4>
-        <div className="Home-description">
-          the recipient receives an email with a link and code to view their letters
-        </div>
-        <hr className="Home-line" />
-        <h4 className="Home-subTitle Home-blueText">6. recipient views email!</h4>
-        <div className="Home-description">
-          log in, navigate to the "mailbox" page, click on "add a package id" button and paste in
+          click "get started", log in, navigate to the "mailbox" page, click on "add a package id" button and paste in
           code from email
         </div>
         <hr className="Home-line" />
@@ -79,18 +65,12 @@ class HowItWorks extends Component {
           it's in their junk inbox, sadly. sender is: fromopenwhen@gmail.com
         </div>
         <hr className="Home-line" />
-        <h4 className="Home-subTitle Home-blueText">why won't it let me click the button?</h4>
-        <div className="Home-description">
-          the form requires certain formatting or it won't let you move on, check the instructions
-          and try again (note: MM/DD/YYYY for date!)
-        </div>
+        <br></br>
+        <h1 className="Home-title u-textCenter">about us</h1>
         <hr className="Home-line" />
-        <h4 className="Home-subTitle Home-blueText">why did you make this site?</h4>
+        <h4 className="Home-subTitle Home-blueText">An Jimenez and Kate Pearce </h4>
         <div className="Home-description">
-          we are An and Kate, two gals who believe that small acts of kindness and thoughtful
-          messages can brighten people's lives. we hope that this simple site encourages you to
-          support, inspire, encourage, and celebrate your friends, family, employees, coworkers, or
-          anyone at all!
+        We are two MIT '21 gals who believe that small acts of kindness and thoughtful messages can brighten people's lives. We hope that this simple site encourages you to support, inspire, encourage, and celebrate your friends, family, employees, coworkers, or anyone at all!
         </div>
       </>
     );
