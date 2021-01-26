669e10829a3e641789c2a97b9d4d554562220bec
kmpearce <65831324+kmpearce@users.noreply.github.com>
2021-01-26 14:52:54 -0500

    seafoam fixed footer

---
+++ client/src/components/modules/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className = "Footer-footer u-textCenter">
+      <h4 > open when | created jan 2021 by An Jimenez and Kate Pearce | contact us at fromopenwhen@gmail.com </h4>
+    </div>
+  );
+}
+
+export default Footer;
