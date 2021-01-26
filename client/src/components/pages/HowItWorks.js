6bdf71c9d4602e34e8b761bd333e934e68ad57e2
An Jimenez <anjimenez@Ans-MacBook-Pro-2.local>
2021-01-26 11:11:12 -0800

    how it works side bar attempt (ugly)

--- client/src/components/pages/HowItWorks.js
+++ client/src/components/pages/HowItWorks.js
@@ -2,17 +2,21 @@ import React, { Component } from "react";
 import { Link } from "@reach/router";
 
 import "../../utilities.css";
-import "./Home.css";
+import "./HowItWorks.css";
 
 class HowItWorks extends Component {
   constructor(props) {
     super(props);
   }
 
-render() {
-return (
-    <>
-        <h1 className="Home-title u-textCenter" >how it works</h1>
+  render() {
+    return (
+      <>
+        <h1 className="Home-title u-textCenter u-margin">how it works</h1>
+        <div class="sidenav">
+          <a href="#">how it works</a>
+          <a href="#">about us</a>
+        </div>
         <hr className="Home-line" />
         <h4 className="Home-subTitle Home-blueText">1. login</h4>
         <div className="Home-description">
@@ -41,26 +45,33 @@ return (
         <hr className="Home-line" />
         <h4 className="Home-subTitle Home-blueText">6. recipient views email!</h4>
         <div className="Home-description">
-          log in, navigate to the "mailbox" page, click on "add a package id" button and paste in code from email 
+          log in, navigate to the "mailbox" page, click on "add a package id" button and paste in
+          code from email
         </div>
         <hr className="Home-line" />
-        
+
         <br></br>
         <h1 className="Home-title u-textCenter">faq</h1>
         <hr className="Home-line" />
         <h4 className="Home-subTitle Home-blueText">what is a package? what is a letter?</h4>
         <div className="Home-description">
-          each letter has a prompt, "open when" date, and a message. when you make multiple letters at once for the same person, they are sent as a package via one email that has a link to view them all.
+          each letter has a prompt, "open when" date, and a message. when you make multiple letters
+          at once for the same person, they are sent as a package via one email that has a link to
+          view them all.
         </div>
         <hr className="Home-line" />
         <h4 className="Home-subTitle Home-blueText">what is an "open when" date?</h4>
         <div className="Home-description">
-          a letter's message only becomes visible upon the "open when" date, but you can also set the date to 00/00/0000 for it to be unlocked from the get-go
+          a letter's message only becomes visible upon the "open when" date, but you can also set
+          the date to 00/00/0000 for it to be unlocked from the get-go
         </div>
         <hr className="Home-line" />
-        <h4 className="Home-subTitle  Home-blueText">what will the recipient see if they try to view the letter before the "open when" date?</h4>
+        <h4 className="Home-subTitle  Home-blueText">
+          what will the recipient see if they try to view the letter before the "open when" date?
+        </h4>
         <div className="Home-description">
-          clicking on the closed envelope and prompt will take them to the letter-viewing page, but it will tell them when it will be unlocked instead of showing them the message
+          clicking on the closed envelope and prompt will take them to the letter-viewing page, but
+          it will tell them when it will be unlocked instead of showing them the message
         </div>
         <hr className="Home-line" />
         <h4 className="Home-subTitle Home-blueText">why can't the recipient find the email?</h4>
@@ -70,12 +81,16 @@ return (
         <hr className="Home-line" />
         <h4 className="Home-subTitle Home-blueText">why won't it let me click the button?</h4>
         <div className="Home-description">
-          the form requires certain formatting or it won't let you move on, check the instructions and try again (note: MM/DD/YYYY for date!)
+          the form requires certain formatting or it won't let you move on, check the instructions
+          and try again (note: MM/DD/YYYY for date!)
         </div>
         <hr className="Home-line" />
         <h4 className="Home-subTitle Home-blueText">why did you make this site?</h4>
         <div className="Home-description">
-          we are An and Kate, two gals who believe that small acts of kindness and thoughtful messages can brighten people's lives. we hope that this simple site encourages you to support, inspire, encourage, and celebrate your friends, family, employees, coworkers, or anyone at all! 
+          we are An and Kate, two gals who believe that small acts of kindness and thoughtful
+          messages can brighten people's lives. we hope that this simple site encourages you to
+          support, inspire, encourage, and celebrate your friends, family, employees, coworkers, or
+          anyone at all!
         </div>
       </>
     );
