9a077031db7a39741548eaceef2bc64bfbf12e11
kmpearce <65831324+kmpearce@users.noreply.github.com>
2021-01-27 10:30:07 -0500

    fake centering footer text

--- client/src/components/modules/Footer.js
+++ client/src/components/modules/Footer.js
@@ -3,8 +3,8 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <div className = "Footer-footer u-textCenter">
-      <h4 > open when | created jan 2021 by An Jimenez and Kate Pearce | contact us at fromopenwhen@gmail.com </h4>
+    <div className = "Footer-footerContainer">
+      <h4 className = "Footer-footerText"> open when   |   created jan 2021 by An Jimenez and Kate Pearce   |   contact us at fromopenwhen@gmail.com</h4>
     </div>
   );
 }
